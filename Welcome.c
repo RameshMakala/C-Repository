33489543ecc0c00130804df22a45fbb0ce3683b4
RameshMakala <46078802+RameshMakala@users.noreply.github.com>
2019-08-30 14:19:08 +0530

    Create Welcome.c

---
+++ Welcome.c
@@ -0,0 +1,5 @@
+/* First Program in C Language. C language programs are compsed with header files, main function followed with user defined functions */
+#inclldue<stdion.h>
+void main() {
+  printf("welcome to c programming");
+  }
